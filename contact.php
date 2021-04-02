42e2865d5cc78ac825109a4a1ab97d62d913e8e2
Sushant Mohite <69734778+Darkseidsam7@users.noreply.github.com>
2021-04-02 19:59:36 +0530

    First Verion Hostel MGMT

---
+++ contact.php
@@ -0,0 +1,9 @@
+<h1 style="color:#000000"><b><center>Contact Details</center></b></h1>
+<div align="center">
+<p><b>Address :</b> ABC Engineering College, XYZ Group, Near AIIMS, New Delhi.
+<br>
+<b>Telegram :</b> <a href="https://t.me/samtpoint_resource">Join For Free Resources</a>
+</p>
+<br>
+</p>
+</div>
